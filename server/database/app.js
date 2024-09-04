4840517db5bcbc2c6842d0d9405a0ed9b959907d
ManavShah2400 <shahmanaviant@gmail.com>
2024-09-04 18:33:12 -0400

    Module 3.1

--- server/database/app.js
+++ server/database/app.js
@@ -59,16 +59,34 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
 //Write your code here
+try {
+    const dealers = await Dealerships.find(); // Fetch all dealers from the database
+    res.status(200).json(dealers);
+} catch (err) {
+    res.status(500).json({ message: err.message });
+}
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
+try {
+    const dealers = await Dealerships.find({ state: req.params.state });
+    res.status(200).json(dealers);
+} catch (err) {
+    res.status(500).json({ message: err.message });
+}
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
+try {
+    const documents = await Dealerships.find({id: req.params.id});
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 //Express route to insert review
